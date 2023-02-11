14b870584042ec2c3a42e61f5f26f9a6f89ce0aa
Aditya Shinde <adityashinde4580@gmail.com>
2023-02-12 01:20:55 +0530

    feat: MainActivity.kt

--- app/src/main/java/com/example/implicit_intent/MainActivity.kt
+++ app/src/main/java/com/example/implicit_intent/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.implicit_intent
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.cardview.widget.CardView
 
 class MainActivity : AppCompatActivity() {
@@ -12,5 +15,17 @@ class MainActivity : AppCompatActivity() {
         val webButton = findViewById<CardView>(R.id.cardWeb)
 
         val cameraButton = findViewById<CardView>(R.id.cardCamera)
+
+        webButton.setOnClickListener {
+            val intent = Intent (Intent.ACTION_VIEW)
+            intent.data = Uri.parse("https://adityaashinde.github.io/portfolio/")
+            startActivity(intent)
+        }
+
+        cameraButton.setOnClickListener {
+            val intent = Intent (MediaStore.ACTION_IMAGE_CAPTURE)
+            startActivity(intent)
+        }
+
     }
 }
